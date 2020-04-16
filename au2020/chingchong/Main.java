c0575e93ed80c218dd15f4fc46cc6c619645d3b7
nvkabanenkov <57075763+nvkabanenkov@users.noreply.github.com>
2020-04-16 13:25:57 +0300

    Fixed typo.

--- au2020/chingchong/Main.java
+++ au2020/chingchong/Main.java
@@ -2,8 +2,8 @@ package au2020.chingchong;
 
 public class Main {
     public static void main(String[] args) {
-        var fooClass = new ChingChonger();
-        new Thread(fooClass).start();
-        new Thread(fooClass).start();
+        var chingChonger = new ChingChonger();
+        new Thread(chingChonger).start();
+        new Thread(chingChonger).start();
     }
 }
